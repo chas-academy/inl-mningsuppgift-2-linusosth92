6350fb0f2c5c1233e9230096a117cfde95800c37
linusosth92 <linus.osth92@gmail.com>
2026-05-05 18:25:30 +0200

    klar inlämning 2

--- src/script.js
+++ src/script.js
@@ -1,26 +1,37 @@
 // Definera array av namn
-let people;
+let people = ["Linus", "Melvin", "Evelina"];
 
 
 export function initSearch(people) { // rör ej denna rad
 
-  // Hämta HTML-element
-  
-  // Lägg till en klickhändelse på "Sök"-knappen
-  searchBtn.addEventListener("click",() => {
+// Hämta HTML-element
+
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+const resultDisplay = document.getElementById("resultDisplay");
+
+// Lägg till en klickhändelse på "Sök"-knappen
+
+searchBtn.addEventListener("click",() => {
   
     // 1. Hämta texten i searchInput.value
-    
+    const searchValue = searchInput.value;
     // 2. Skapa en boolean för found
-    
+    let found = false;
     // 3. Loopa igenom people-arrayen
-    
+    for (let person of people) {
     // 4. Jämför texten med varje namn i arrayen
-    
+    if (person === searchValue) {
     // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
-    
+    resultDisplay.innerText = "Namn hittades!";
+        found = true;
+        break;
+      }
+    }
     // 6. Om du efter loopen inte hittat något, skriv ut "Namn hittades inte." och gör found till false igen.
-    
+    if (!found) {
+      resultDisplay.innerText = "Namn hittades inte.";
+    }
     });
 }
 
